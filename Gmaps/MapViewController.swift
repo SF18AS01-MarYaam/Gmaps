d46bc7e1411de7fd25f0653bbdc552713cb3923b
MarYaam <yaamnjie@gmail.com>
2019-01-08 21:31:15 -0500

    switched map with address autocomplete form

--- Gmaps/MapViewController.swift
+++ Gmaps/MapViewController.swift
@@ -7,53 +7,147 @@
 //
 
 import UIKit
-import GoogleMaps
+import GooglePlaces
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var mapView: GMSMapView!
+    
+    // Declare UI elements.
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var address_line_1: UITextField!
+    
+    
+    @IBOutlet weak var address_line_2: UITextField!
+    
+    
+    @IBOutlet weak var city: UITextField!
+    
+    
+    @IBOutlet weak var state: UITextField!
+    
+    
+    @IBOutlet weak var postal_code_field: UITextField!
+    
+    
+    @IBOutlet weak var country_field: UITextField!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    // Declare variables to hold address form values.
+    var street_number: String = ""
+    var route: String = ""
+    var neighborhood: String = ""
+    var locality: String = ""
+    var administrative_area_level_1: String = ""
+    var country: String = ""
+    var postal_code: String = ""
+    var postal_code_suffix: String = ""
+    
+    // Present the Autocomplete view controller when the user taps the search field.
+    
+    
+    
+    
+    @IBAction func autocompleteClicked(_ sender: UIButton) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
         
-        let camera = GMSCameraPosition.camera(withLatitude: 37.36, longitude: -122.0, zoom: 6.0)
-        mapView.camera = camera
-        showMarker(position: camera.target)
-        mapView.delegate = self
+        // Set a filter to return only addresses.
+        let addressFilter = GMSAutocompleteFilter()
+        addressFilter.type = .address
+        autocompleteController.autocompleteFilter = addressFilter
         
-
+        present(autocompleteController, animated: true, completion: nil)
     }
     
-    func showMarker(position: CLLocationCoordinate2D){
-        let marker = GMSMarker()
-        marker.position = position
-        marker.title = "Palo Alto"
-        marker.snippet = "San Francisco"
-        marker.map = mapView
+    // Populate the address form fields.
+    func fillAddressForm() {
+        address_line_1.text = street_number + " " + route
+        city.text = locality
+        state.text = administrative_area_level_1
+        if postal_code_suffix != "" {
+            postal_code_field.text = postal_code + "-" + postal_code_suffix
+        } else {
+            postal_code_field.text = postal_code
+        }
+        country_field.text = country
+        
+        // Clear values for next time.
+        street_number = ""
+        route = ""
+        neighborhood = ""
+        locality = ""
+        administrative_area_level_1  = ""
+        country = ""
+        postal_code = ""
+        postal_code_suffix = ""
     }
-    
     
 }
 
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        // Print place info to the console.
+        print("Place name: \(place.name)")
+        print("Place address: \(String(describing: place.formattedAddress))")
+        print("Place attributions: \(String(describing: place.attributions))")
 
-extension ViewController: GMSMapViewDelegate{
-    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D){
-        let marker = GMSMarker()
-        marker.position = coordinate
+        
+        // Get the address components.
+        if let addressLines = place.addressComponents {
+            // Populate all of the address fields we can find.
+            for field in addressLines {
+                switch field.type {
+                case kGMSPlaceTypeStreetNumber:
+                    street_number = field.name
+                case kGMSPlaceTypeRoute:
+                    route = field.name
+                case kGMSPlaceTypeNeighborhood:
+                    neighborhood = field.name
+                case kGMSPlaceTypeLocality:
+                    locality = field.name
+                case kGMSPlaceTypeAdministrativeAreaLevel1:
+                    administrative_area_level_1 = field.name
+                case kGMSPlaceTypeCountry:
+                    country = field.name
+                case kGMSPlaceTypePostalCode:
+                    postal_code = field.name
+                case kGMSPlaceTypePostalCodeSuffix:
+                    postal_code_suffix = field.name
+                // Print the items we aren't using.
+                default:
+                    print("Type: \(field.type), Name: \(field.name)")
+                }
+            }
+        }
+        
+        // Call custom function to populate the address form.
+        fillAddressForm()
+        
+        // Close the autocomplete widget.
+        self.dismiss(animated: true, completion: nil)
     }
-    //MARK - GMSMarker Dragging
-    func mapView(_ mapView: GMSMapView, didBeginDragging marker: GMSMarker) {
-        print("didBeginDragging")
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
     }
-    func mapView(_ mapView: GMSMapView, didDrag marker: GMSMarker) {
-        print("didDrag")
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
     }
-    func mapView(_ mapView: GMSMapView, didEndDragging marker: GMSMarker) {
-        print("didEndDragging")
+    
+    // Show the network activity indicator.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    // Hide the network activity indicator.
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
     
 }
-
-
 
